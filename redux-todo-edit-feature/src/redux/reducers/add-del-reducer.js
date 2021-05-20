7e587bdbc760b857974827b25dfe30b385096638
munsif12 <munsifalimisri69@gmail.com>
2021-05-20 22:38:39 +0500

    all reducers like updated,delete,edit,insert all are done

--- redux-todo-edit-feature/src/redux/reducers/add-del-reducer.js
+++ redux-todo-edit-feature/src/redux/reducers/add-del-reducer.js
@@ -1,9 +1,11 @@
 const ADD_ITEM = "ADD_ITEM";
 const DELETE_ITEM = "DELETE_ITEM";
 const EDIT_ITEM = "EDIT_ITEM";
+const UPDATED = "UPDATED";
 const initialState = {
   list: [],
 };
+let idForEdit = 0;
 const add_del_reducers = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ITEM:
@@ -30,6 +32,15 @@ const add_del_reducers = (state = initialState, action) => {
         list: filterArr,
       };
     case EDIT_ITEM:
+      idForEdit = action.payload.id; //ya jo id lakr arha h isko ham global krrhy ha taka updateed reducer ka waqt ham is id ka use karty huay apna content match krky usko ko specificaly update karaky
+      return {
+        list: state.list,
+      };
+    case UPDATED:
+      const objIndex = state.list.findIndex((obj) => obj.id === idForEdit); //goood logic to update specific element
+      // console.log("Before update: ", state.list[objIndex]);
+      state.list[objIndex].data = action.payload.data;
+      // console.log("After update: ", state.list[objIndex]);
       return {
         list: state.list,
       };
