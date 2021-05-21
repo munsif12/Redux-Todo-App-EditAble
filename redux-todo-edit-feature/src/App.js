e682541c353eebf20c4127c2937ca9d1b8fae7e3
munsif12 <munsifalimisri69@gmail.com>
2021-05-21 13:48:24 +0500

    React-reduc-todo-app-with-updation =>-done

--- redux-todo-edit-feature/src/App.js
+++ redux-todo-edit-feature/src/App.js
@@ -9,6 +9,8 @@ import {
 } from "./redux/actions/actions";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import EditIcon from "@material-ui/icons/Edit";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 function App() {
   const data = useSelector((state) => state.add_del_reducers.list);
   const dispatch = useDispatch();
@@ -21,81 +23,99 @@ function App() {
     localStorage.setItem("list", JSON.stringify(listItem));
   }, [listItem]);
   return (
-    <div className="App">
-      <h1>React Redux Todo App</h1>
-      <div className="input">
-        <input
-          type="text"
-          name="item"
-          id="text"
-          value={listItem}
-          onChange={(e) => setListItem(e.target.value)}
-          ref={inputFocus}
-        />
-        {!editorNot ? (
-          <button
-            onClick={() => {
-              dispatch(add_item(listItem));
-              setListItem(" ");
-              inputFocus.current.focus();
-            }}
-          >
-            +
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              seteditorNot(false);
-              dispatch(updated(listItem));
-              setListItem(" ");
-              inputFocus.current.focus();
-            }}
-          >
-            edit
-          </button>
-        )}
-      </div>
-      <div className="allLists">
-        <div className="allItmes_wrapper">
-          <ul>
-            {data.map((val, index) => {
-              //displaying all the lists with map
-              return (
-                <div className={`listWrapper${index + 1} listWrapper`}>
-                  <li className={`item${index + 1} item`}>
-                    <div className="item">
-                      {" "}
-                      <p>{val.data}</p>{" "}
-                    </div>
-                    <EditIcon
-                      style={{
-                        cursor: "pointer",
-                        color: "green",
-                        transform: "scale(0.8)",
-                      }}
-                      onClick={() => {
-                        dispatch(edit_item(val.id));
-                        setListItem(val.data, listItem);
-                        seteditorNot(true);
-                        inputFocus.current.focus();
-                      }}
-                    />
-                    <DeleteForeverIcon
-                      style={{
-                        cursor: "pointer",
-                        color: "red",
-                        transform: "scale(0.8)",
-                      }}
-                      onClick={() => dispatch(delete_item(val.id))}
-                    />
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
+    <>
+      <div className="black top"></div>
+      <div className="black center"></div>
+      <div className="black bottom"></div>
+      <div className="App">
+        <div className="heading">
+          <h1>React Redux Todo App</h1>
+        </div>
+        <div className="inputFields">
+          <input
+            type="text"
+            name="item"
+            id="text"
+            value={listItem}
+            onChange={(e) => setListItem(e.target.value)}
+            ref={inputFocus}
+            placeholder="Type sonething here..."
+            autoComplete="off"
+          />
+          {!editorNot ? (
+            <button
+              className="addBtn common"
+              onClick={() => {
+                if (listItem.length >= 3) {
+                  dispatch(add_item(listItem));
+                  setListItem(" ");
+                  inputFocus.current.focus();
+                } else {
+                  alert("Value Must be greater then 3 character");
+                }
+              }}
+            >
+              <AddCircleOutlineIcon />
+            </button>
+          ) : (
+            <button
+              className="editBtn common"
+              onClick={() => {
+                if (listItem.length >= 3) {
+                  seteditorNot(false);
+                  dispatch(updated(listItem));
+                  setListItem(" ");
+                  inputFocus.current.focus();
+                } else {
+                  alert("Value Must be greater then 3 character");
+                }
+              }}
+            >
+              <CheckCircleOutlineIcon />
+            </button>
+          )}
+        </div>
+        <div className="allLists">
+          <div className="allItmes_wrapper">
+            <ul>
+              {data.map((val, index) => {
+                //displaying all the lists with map
+                return (
+                  <div className={`listWrapper${index + 1} listWrapper`}>
+                    <li className={`item${index + 1} item`}>
+                      <div className="item">
+                        <p>{val.data}</p>{" "}
+                      </div>
+                      <EditIcon
+                        style={{
+                          cursor: "pointer",
+                          color: "green",
+                          transform: "scale(0.8)",
+                        }}
+                        onClick={() => {
+                          dispatch(edit_item(val.id));
+                          setListItem(val.data, listItem);
+                          seteditorNot(true);
+                          inputFocus.current.focus();
+                        }}
+                      />
+                      <DeleteForeverIcon
+                        style={{
+                          cursor: "pointer",
+                          color: "red",
+                          transform: "scale(0.8)",
+                        }}
+                        onClick={() => dispatch(delete_item(val.id))}
+                      />
+                    </li>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
